7735e837074a743b56513b0b8f3e965479a47b13
Bhumika Burhade <bhumikaburhade26@gmail.com>
2026-03-29 14:48:10 +0530

    Day 2 :Learnt array map and rendering to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const pipeline = [{
+  id:1 , 
+  name : 'Pipeline 1',
+  status : 'success!✅'
+},{
+id: 2,
+name : 'Pipeline 2',
+status : 'Failed ❌'
+},{
+  id:3,
+  name: 'Pipeline 3',
+  status: 'Running 🏃‍♀️'
+}]
+
 function App() {
   return (
     <div>
       <h1>MY CI/CD DASHBOARD!</h1>
+      {pipeline.map((pl) =>(
+      <p key ={pl.id}>
+        {pl.name} - {pl.status}
+      </p>
+
+      ))}
     </div>
   );
 }
